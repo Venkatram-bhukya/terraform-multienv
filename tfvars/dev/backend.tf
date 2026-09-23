7604c322b4e788ef484b9da077fc8bb80cbaefd0
bhukyadevops@gmail.com <bhukyadevops@gmail.com>
2026-09-23 13:15:38 +0530

    terraform

--- tfvars/dev/backend.tf
+++ tfvars/dev/backend.tf
@@ -1,4 +1,4 @@
-bucket  = "remote-state-20s-dev" # Replace with your unique bucket name
+bucket  = "remote-state-aws-20s-dev.1" # Replace with your unique bucket name
 key     = "remote-state.tfstate"
 region  = "us-east-1"
 encrypt = true
--- tfvars/prod/backend.tf
+++ tfvars/prod/backend.tf
@@ -1,4 +1,4 @@
-bucket  = "remote-state-20s-prod" # Replace with your unique bucket name
+bucket  = "remote-state-aws-20s-prod.1" # Replace with your unique bucket name
 key     = "remote-state.tfstate"
 region  = "us-east-1"
 encrypt = true
